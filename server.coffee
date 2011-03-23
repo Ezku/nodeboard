5ad841b180835dc760b12f30bf1d4656f64dfc7b
ezku <eevert.saukkokoski@brainalliance.com>
2011-03-23 13:44:33 +0200

    adding CS compilation to server script for Duostack compatibility

--- server.coffee
+++ server.coffee
@@ -1,5 +1,13 @@
-app = require(__dirname + '/app/bootstrap.js')(__dirname)
+{exec} = require 'child_process'
+console.log "Compiling Coffeescript files..."
+exec "coffee -c #{__dirname}", (error, stdout, stderr) ->
+  console.log stdout if stdout
+  console.log stderr if stderr
+  throw error if error
+  
+  app = require(__dirname + '/app/bootstrap.js')(__dirname)
 
-unless module.parent
-  app.listen 3000
-  console.log "Express server listening on port %d", app.address().port
+  unless module.parent
+    app.listen 3000
+    console.log "Express server listening on port %d", app.address().port
+
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 (function() {
-  var app;
-  app = require('./app/bootstrap.js')(__dirname);
-  if (!module.parent) {
-    app.listen(3000);
-    console.log("Express server listening on port %d", app.address().port);
-  }
+  var exec;
+  exec = require('child_process').exec;
+  console.log("Compiling Coffeescript files...");
+  exec("coffee -c " + __dirname, function(error, stdout, stderr) {
+    var app;
+    if (stdout) {
+      console.log(stdout);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    if (error) {
+      throw error;
+    }
+    app = require(__dirname + '/app/bootstrap.js')(__dirname);
+    if (!module.parent) {
+      app.listen(3000);
+      return console.log("Express server listening on port %d", app.address().port);
+    }
+  });
 }).call(this);
