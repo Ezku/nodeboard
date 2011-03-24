0a79d76096ce474e0d9cda3fccf6edec6c7891f0
Ossi Hanhinen <hanhinen@gmail.com>
2011-03-24 09:40:55 +0200

    Header added + some styling done

--- app/views/layout.coffee
+++ app/views/layout.coffee
@@ -5,4 +5,36 @@ html ->
     link rel: "stylesheet", href: '/stylesheets/style.css'
     script src: '/scripts/modernizr-1.7.min.js'
   body ->
-    @body
+    div id: "page-wrapper", ->
+      header ->
+        nav ->
+          ul ->
+            li ->
+              a href: ".", -> "Aaltoboard"
+            li ->
+              a href: "ak/", -> "AK"
+            li ->
+              a href: "as/", -> "AS"
+            li ->
+              a href: "bio/", -> "BIO"
+            li ->
+              a href: "fk/", -> "FK"
+            li ->
+              a href: "ik/", -> "IK"
+            li ->
+              a href: "inf/", -> "INF"
+            li ->
+              a href: "kik/", -> "KIK"
+            li ->
+              a href: "kk/", -> "KK"
+            li ->
+              a href: "pjk/", -> "PJK"  
+            li ->
+              a href: "tik/", -> "TIK"
+            li ->
+              a href: "vk/", -> "VK"
+      div id: "column-wrapper", ->
+        section id: "high-level", ->
+          @body
+        section id: "detail", ->
+          ""
--- app/views/layout.js
+++ app/views/layout.js
@@ -12,7 +12,114 @@
       });
     });
     return body(function() {
-      return this.body;
+      return div({
+        id: "page-wrapper"
+      }, function() {
+        header(function() {
+          return nav(function() {
+            return ul(function() {
+              li(function() {
+                return a({
+                  href: "."
+                }, function() {
+                  return "Aaltoboard";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "ak/"
+                }, function() {
+                  return "AK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "as/"
+                }, function() {
+                  return "AS";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "bio/"
+                }, function() {
+                  return "BIO";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "fk/"
+                }, function() {
+                  return "FK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "ik/"
+                }, function() {
+                  return "IK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "inf/"
+                }, function() {
+                  return "INF";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "kik/"
+                }, function() {
+                  return "KIK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "kk/"
+                }, function() {
+                  return "KK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "pjk/"
+                }, function() {
+                  return "PJK";
+                });
+              });
+              li(function() {
+                return a({
+                  href: "tik/"
+                }, function() {
+                  return "TIK";
+                });
+              });
+              return li(function() {
+                return a({
+                  href: "vk/"
+                }, function() {
+                  return "VK";
+                });
+              });
+            });
+          });
+        });
+        return div({
+          id: "column-wrapper"
+        }, function() {
+          section({
+            id: "high-level"
+          }, function() {
+            return this.body;
+          });
+          return section({
+            id: "detail"
+          }, function() {
+            return "";
+          });
+        });
+      });
     });
   });
 }).call(this);
